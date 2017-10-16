367347b1dc75ca662664a517f4862d68ec3ede3a
n9 <n9>
2017-10-16 14:30:58 +0200

    wip Issue873Tests

--- Tests/Linq/UserTests/Issue873Tests.cs
+++ Tests/Linq/UserTests/Issue873Tests.cs
@@ -5,6 +5,7 @@ using LinqToDB;
 
 namespace Tests.UserTests
 {
+	[TestFixture]
 	public class Issue873Tests : TestBase
 	{
 		[Test, DataContextSource]
@@ -21,6 +22,26 @@ namespace Tests.UserTests
 						{
 							Label = " " + e.Value1,
 							Count = q.Where(_ => _.Parent == e).Count()
+						},
+					})
+					.Where(_ => _.Fields.Label.Contains("1"));
+
+				var array = query.ToArray();
+			}
+		}
+
+		[Test, DataContextSource]
+		public void Test2(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var query = db.Parent	
+					.Select(e => new
+					{
+						Fields = new
+						{
+							Label = " " + e.Value1,
+							Label2 = " " + e.Value1,
 						},
 					})
 					.Where(_ => _.Fields.Label.Contains("1"));
